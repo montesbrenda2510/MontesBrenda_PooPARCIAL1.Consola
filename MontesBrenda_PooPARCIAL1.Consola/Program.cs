98c95963c32d4d58f781b2703656504b8a0e57af
Brendi <Brendi@BRENDI>
2023-07-10 19:59:58 -0300

    Agregar archivos de proyecto.

---
+++ MontesBrenda_PooPARCIAL1.Consola/Program.cs
@@ -0,0 +1,50 @@
+﻿using MontesBrenda_PooPARCIAL1.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MontesBrenda_PooPARCIAL1.Consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Crear una priramide");
+            var ladodelabase = PedirInt("Ingrese el lado de la base: ");
+            var altura = PedirInt("Ingrese la altura: ");
+
+            Piramide piramide = new Piramide(ladodelabase, altura);
+
+            Console.WriteLine("Informacion de la piramide");
+            Console.WriteLine($"la arista es:{piramide.GetCalcularArista()}");
+            Console.WriteLine($"El Area es: {piramide.GetArea()}");
+            Console.WriteLine($"El volumen es: {piramide.GetVolumen()}");
+
+            Console.ReadLine();
+
+
+        }
+
+        private static int PedirInt(string mensaje)
+        {
+            int nro;
+            do
+            {
+                string str;
+                Console.Write(mensaje);
+                str = Console.ReadLine();
+
+                if (int.TryParse(str, out nro))
+                {
+                    break;
+                }
+                Console.WriteLine("Número no válido");
+
+            } while (true);
+            return nro;
+
+        }
+    }
+}
---
+++ MontesBrenda_PooPARCIAL1.Entidades/Piramide.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MontesBrenda_PooPARCIAL1.Entidades
+{
+    public class Piramide
+    {
+
+        private int ladodelabase;
+        private int altura;
+        
+        public int Ladodelabase
+        {
+            get { return ladodelabase; }
+            set { ladodelabase = value; }
+
+        }
+        
+        public Piramide(int altura, int ladodelabase)
+        {
+            altura = altura;
+            ladodelabase = ladodelabase;
+            GetCalcularArista(); 
+        }
+       
+
+        public double GetCalcularArista() => Math.Sqrt(Math.Pow(altura,2) + Math.Pow(ladodelabase,2));
+
+        public double GetArea() => ladodelabase * (Ladodelabase + Math.Sqrt(Math.Pow(altura, 2) + Math.Pow(ladodelabase, 2)));
+
+
+        public double GetVolumen() => (Math.Pow(ladodelabase, 2) * altura) / 3;
+        
+        
+        }
+}
+
